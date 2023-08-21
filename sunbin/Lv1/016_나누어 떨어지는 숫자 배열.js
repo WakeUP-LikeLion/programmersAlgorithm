5b182e899e5872fe413d2a322f44654764e22d88
jsunbin <jsnb6n@gmail.com>
2023-08-21 12:29:49 +0900

    💻 Day35 - Lv1 나누어 떨어지는 숫자 배열

--- sunbin/Lv1/016_나누어 떨어지는 숫자 배열.js
+++ sunbin/Lv1/016_나누어 떨어지는 숫자 배열.js
@@ -2,5 +2,21 @@
 
 function solution(arr, divisor) {
   const answer = [];
-  return answer;
+
+  for (const i of arr) {
+
+    if (i % divisor === 0) {
+      answer.push(i)
+    }
+  }
+  
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
+}
+
+// 다른 풀이: filter
+
+function solution(arr, divisor) {
+    var answer = arr.filter(v => v % divisor == 0);
+    
+    return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
 }
