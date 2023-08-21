8353183d88df608bbf730e8a184e95ed2dfefe09
jsunbin <jsnb6n@gmail.com>
2023-08-21 12:29:22 +0900

    💻 Day35 - Lv1 서울에서 김서방 찾기

--- sunbin/Lv1/015_서울에서 김서방 찾기.js
+++ sunbin/Lv1/015_서울에서 김서방 찾기.js
@@ -1,6 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12919
 
 function solution(seoul) {
-  const answer = '';
+  const index = seoul.indexOf("Kim");
+  
+  const answer = `김서방은 ${index}에 있다`;
   return answer;
 }
